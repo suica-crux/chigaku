e4526b38ff7895aa061871730ba3391c1fd158a0
i03-Dinar <dinar@vipelar.com>
2025-04-18 05:52:57 +0000

    (略)/[topic]/page.tsxを更新

--- src/app/manual/[category]/[topic]/page.tsx
+++ src/app/manual/[category]/[topic]/page.tsx
@@ -4,14 +4,14 @@ import path from 'path';
 import Heading from '@/app/components/Heading';
 import { marked } from 'marked';
 
-type Topics = {
+interface Params {
   params: {
     category: string;
     topic: string;
   };
-};
+}
 
-const TopicPage = async ({ params }: Topics) => {
+export default async function TopicPage({ params }: Params) {
   const { category, topic } = params;
   const categoryPath = path.join(process.cwd(), `src/app/manual/${category}`);
   const topicPath = path.join(categoryPath, topic);
@@ -58,5 +58,3 @@ const TopicPage = async ({ params }: Topics) => {
     </div>
   );
 }
-
-export default TopicPage;
