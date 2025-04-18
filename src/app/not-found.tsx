a087d8f3011a2e9cba1b1868f12ec3f0053b2d28
i03-Dinar <dinar@vipelar.com>
2025-04-18 05:13:24 +0000

    不要なログを削除

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,6 @@ import NotFoundComponent from './NotFoundComponent';
 const metadata: Metadata = {
   title: 'ページが見つかりません(404) - 同志社高校地学部',
 };
-console.log(metadata);
 
 export default function NotFound() {
   return (
