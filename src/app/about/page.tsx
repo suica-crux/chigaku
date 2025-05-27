eb9a110f52f689bea4f8bed5404210bbd2c47112
suica-crux <autumn@vipelar.com>
2025-05-27 15:08:20 +0900

    Contenttt (#38)
    * aboutとcontactを追加
    
    * ぎっといぐのあー

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Heading from '@/components/Heading';
+import Text from '@/components/Text';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Heading title="地学部について" />
+      <Text>地学部の紹介文を書いてもらおっと</Text>
+
+      <Text size="lg">企画課 人員募集について</Text>
+      <Text>詳細</Text>
+    </div>
+  );
+}
--- src/data/titles.ts
+++ src/data/titles.ts
@@ -12,4 +12,7 @@ export const titles: Record<string, string> = {
   finder: 'ファインダーの合わせ方',
   lens: '接眼レンズの替え方',
   stair: '階段のふたの閉じ方',
+  // other
+  about: '地学部について',
+  contact: 'お問い合わせ',
 };
