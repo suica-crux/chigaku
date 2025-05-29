20f427b4b27ddbe77098136d1e04f28491ace0f4
akika <autumn@vipelar.com>
2025-05-29 23:37:26 +0900

    内容を追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,30 @@
 import Heading from '@/components/Heading';
 import Text from '@/components/Text';
+import Space from '@/components/Space';
 
 export default function AboutPage() {
   return (
     <div>
       <Heading title="地学部について" />
-      <Text>地学部の紹介文を書いてもらおっと</Text>
+      <Text>
+        同志社高校地学部は、コロナなどの影響でなくなっていた部活を2021年にもう一度立ち上げ、以降続いている部活です。
+        <br />
+        現在、70人近くの生徒が所属している地学部ですが、現時点では月に1度の夜間天体観測(通称:
+        夜天)を行っています。
+        <br />
+        夜天では、万象館にある天体望遠鏡を使って星を観測し、屋上で夜空を観察します。
+      </Text>
+      <Text>
+        夏休みには合宿も行います。去年度(2024年)には兵庫にある西はりま天文台に行き、施設を見学、観察しました。
+      </Text>
+      <Space />
+      <Text size="lg" className="bold">星を見たい君たちへ。一緒に地学部で活動しませんか。</Text>
+      <Space />
+      <Text size="sm">企画課より: 今後平日の活動も増やす予定です。随時更新</Text>
+      {/* <Photo src="" /> */}
 
-      <Text size="lg">企画課 人員募集について</Text>
-      <Text>詳細</Text>
+      {/* <Text size="lg">企画課 人員募集について</Text>
+      <Text>詳細</Text> */}
     </div>
   );
 }
